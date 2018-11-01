b8ba7562358985e58aefdb60603d50a06432fc3d
James Mackenzie <jim@lilandjim.com>
2018-11-01 19:24:53 +0000

    Initial commit
    Initial commit

---
+++ src/pdfmake-test.js
@@ -0,0 +1,51 @@
+window.fontsHaveBeenLoaded = false;
+
+function downloadFonts(callback) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            const vfs = JSON.parse(this.responseText);
+            callback(vfs);
+        }
+    };
+    xhttp.open("GET", "/vfs/vfs.json", true);
+    xhttp.send();
+}
+
+function setupPdfMake(callback) {
+    if (!window.fontsHaveBeenLoaded) {
+        downloadFonts(function (vfs) {
+            pdfMake.vfs = vfs;
+
+            pdfMake.fonts = {
+                KaigenSansJ: {
+                    normal: 'KaigenSansJ-Regular.ttf',
+                    bold: 'KaigenSansJ-Regular.ttf',
+                    italics: 'KaigenSansJ-Regular.ttf',
+                    bolditalics: 'KaigenSansJ-Regular.ttf'
+                }
+            }
+
+            window.fontsHaveBeenLoaded = true;
+
+            callback();
+        });
+    } else {
+        callback();
+    }
+}
+
+function generatePdf(text) {
+    const docDefinition = {
+        content: text,
+        defaultStyle: {
+            font: 'KaigenSansJ'
+        }
+    };
+
+    setupPdfMake(function () {
+        pdfMake.createPdf(docDefinition).getDataUrl(function (outDoc) {
+            window.open(outDoc, 'pdf');
+        });
+    })
+}
